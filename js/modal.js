c073585850b1ce3e565b97ba603b989dae3b1b4f
Bluesman <dmitry.khalimov@gmail.com>
2020-07-15 13:12:44 +0300

    Оживляет модальное окна Заявка принята

--- js/modal.js
+++ js/modal.js
@@ -2,36 +2,79 @@
 
 var callbackOpenButton = document.querySelector('.page-header__callback');
 var callbackModal = document.querySelector('.modal-callback');
+var callbackSubmitButton = document.querySelector('.modal-callback__button');
 var callbackCloseButton = document.querySelector('.modal-callback__close');
+
+var acceptedModal = document.querySelector('.modal-accepted');
+var acceptedCloseButton = document.querySelector('.modal-accepted__close');
+var acceptedSubmitButton = document.querySelector('.modal-accepted__button');
 
 var body = document.querySelector('body');
 
-var onModalOpenClick = function () {
-  openModal();
+var onCallbackOpenClick = function () {
+  openCallback();
 };
 
-var onModalCloseClick = function () {
-  closeModal();
+var onCallbackCloseClick = function () {
+  closeCallback();
 };
 
-var onModalCloseEsc = function (evt) {
+var onCallbackEsc = function (evt) {
   if (evt.key === 'Escape') {
     evt.preventDefault();
-    closeModal();
+    closeCallback();
   }
 };
 
-var openModal = function () {
+var onAcceptedEsc = function (evt) {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    closeAccepted();
+  }
+};
+
+var onCallbackSubmitClick = function (evt) {
+  evt.preventDefault(); // тут должна быть логика отправки на сервер через XHR
+  closeCallback();
+  openAccepted();
+};
+
+var onAcceptedCloseClick = function () {
+  closeAccepted();
+};
+
+var onAcceptedSubmitClick = function () {
+  closeAccepted();
+};
+
+var openCallback = function () {
   body.style.overflow = 'hidden';
   callbackModal.classList.remove('visually-hidden');
-  document.addEventListener('keydown', onModalCloseEsc);
+  document.addEventListener('keydown', onCallbackEsc);
 };
 
-var closeModal = function () {
+var closeCallback = function () {
   body.style.overflow = 'visible';
   callbackModal.classList.add('visually-hidden');
-  document.removeEventListener('keydown', onModalCloseEsc);
+  document.removeEventListener('keydown', onCallbackEsc);
 };
 
-callbackOpenButton.addEventListener('click', onModalOpenClick);
-callbackCloseButton.addEventListener('click', onModalCloseClick);
+var openAccepted = function () {
+  body.style.overflow = 'hidden';
+  acceptedModal.classList.remove('visually-hidden');
+  document.addEventListener('keydown', onAcceptedEsc);
+};
+
+var closeAccepted = function () {
+  body.style.overflow = 'visible';
+  acceptedModal.classList.add('visually-hidden');
+  document.removeEventListener('keydown', onAcceptedEsc);
+};
+
+callbackOpenButton.addEventListener('click', onCallbackOpenClick);
+callbackCloseButton.addEventListener('click', onCallbackCloseClick);
+callbackSubmitButton.addEventListener('click', onCallbackSubmitClick);
+
+acceptedCloseButton.addEventListener('click', onAcceptedCloseClick);
+acceptedSubmitButton.addEventListener('click', onAcceptedSubmitClick);
+
