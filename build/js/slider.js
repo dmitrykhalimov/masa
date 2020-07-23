4242fb92260fed3bf15df2147d8060e9dbea3c82
Bluesman <dmitry.khalimov@gmail.com>
2020-07-23 17:32:35 +0300

    Модифицирован слайдер

--- build/js/slider.js
+++ build/js/slider.js
@@ -14,7 +14,6 @@ buttonsSlider.forEach(function (button, index) {
 
 window.onresize = function () {
   var currentWidth = window.innerWidth;
-  console.log(currentWidth);
   if (currentWidth > 767) {
     returnSlides();
   }
@@ -29,6 +28,8 @@ var returnSlides = function () {
 };
 
 var showSlides = function (numberSlide) {
+  document.querySelector('.life-in-israel__selector--active').classList.remove('life-in-israel__selector--active');
+  buttonsSlider[numberSlide].classList.add('life-in-israel__selector--active');
   var slides = document.querySelectorAll('.life-in-israel__slider-item');
 
   slides.forEach(function (slide, index) {
